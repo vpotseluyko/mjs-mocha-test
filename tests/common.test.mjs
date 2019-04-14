1cce32c1c42072bc6ca0a5b71901db78681381a4
v.potseluyko <vyacheslav.potseluyko@gmail.com>
2019-04-14 09:50:51 +0300

    initial commit

---
+++ tests/common.test.mjs
@@ -0,0 +1,10 @@
+import chai from 'chai';
+
+const { expect } = chai; 
+
+describe('common tests', () => {
+    it('test', () => {
+        let result = 'aaa';
+        expect(result).equal('aaa');
+    });
+});
